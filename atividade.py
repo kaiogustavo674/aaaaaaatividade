d266515c93d7f9abfc0d202eb8fce267bb06a061
LauraIsabeleMatiole <166854387+LauraIsabeleMatiole@users.noreply.github.com>
2024-05-29 11:15:18 -0300

    commitandooo

--- atividade.py
+++ atividade.py
@@ -1,12 +1,26 @@
 print ('projeto fitness!!')
 
+
+
+
+
+
 while True:
  print('tela inicial')
 
- opcao = input('digite a opção que você deseja escolher (1: peso ideal, 2:, 3:  )')
- if opcao == 1:
+
+ opcao = int input('digite a opção que você deseja escolher (1: peso ideal, 2:, 3:  ): '
+ if opcao == (1):
+ 
   print('calcular o peso ideal:')
-  idade = input ('digite ')
+  idade = input ('digite sua altura')
+  if genero == "M":
+   pesoideal == (0.75 × (altura - 152.4))
+   print (f'seu peso ideal é {pesoideal}')
+  elif genero == "F":
+   pesoIdeal = 52 + (0.67 × (altura - 152.4))
+   print (f'seu peso ideal é {pesoideal}')
+
 
  elif opcao == 2:
   print('')
