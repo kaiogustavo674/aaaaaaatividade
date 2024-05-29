42b1c5303514ab3a88efd46a66a64627810cd8fd
LauraIsabeleMatiole <166854387+LauraIsabeleMatiole@users.noreply.github.com>
2024-05-29 10:23:04 -0300

    aaa

--- atividade.py
+++ atividade.py
@@ -3,5 +3,13 @@ print ('projeto fitness!!')
 while True:
  print('tela inicial')
 
- opcao = input('digite a opção que você deseja escolher (1: )')
+ opcao = input('digite a opção que você deseja escolher (1: peso ideal, 2:, 3:  )')
  if opcao == 1:
+  print('calcular o peso ideal:')
+  idade = input ('digite ')
+
+ elif opcao == 2:
+  print('')
+
+ elif opcao == 3:
+  print('  ')
