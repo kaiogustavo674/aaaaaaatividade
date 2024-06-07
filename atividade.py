a43732614e475085a115ad661e47bba9b3fea18e
Laura <matiolelaura649@gmail.com>
2024-06-07 16:23:46 -0300

    frthfhftgh

--- atividade.py
+++ atividade.py
@@ -22,17 +22,4 @@ while True:
  opcao = int (input('digite a opção que você deseja escolher (1: peso ideal, 2:, 3:  '))
  
  if opcao == (1):
- 
- 
-
-
- elif opcao == (2):
-  
- 
-
-
-
- elif opcao == (3):
-  
-
-
+  print('peso ideal')
