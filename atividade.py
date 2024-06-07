8ab74c54a06bc22fbebede93bacb5988c444cc5f
Laura <matiolelaura649@gmail.com>
2024-06-07 16:20:00 -0300

    vaiii

--- atividade.py
+++ atividade.py
@@ -19,20 +19,20 @@
 
 while True:
  print('tela inicial')
-
-
  opcao = int (input('digite a opção que você deseja escolher (1: peso ideal, 2:, 3:  '))
+ 
  if opcao == (1):
  
  
 
 
  elif opcao == (2):
+  
  
 
 
 
  elif opcao == (3):
- 
+  
 
 
