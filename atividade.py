3894d275090ceb4bde5cb67f8c91c6dacc465a89
Samuel Campos <166622578+Sah123campos@users.noreply.github.com>
2024-06-12 07:35:31 -0300

    Update atividade.py

--- atividade.py
+++ atividade.py
@@ -1,2 +1,15 @@
 print ('projeto fitness!!')
-
+while True:
+    print("O que você quer fazer ?")
+    option= input('"Logar","Cadastrar-se"')
+    if option == "Logar":
+        gmail =input("Digite seu gmail")
+        senha =input("digite sua senha")
+    elif option == "Cadastrar-se":
+        cadastro = input("Qual o seu gmail")
+        if cadastro != gmail:
+            print("gmail incorreto")
+            cadastro2 = input("Qual sua senha")
+            if cadastro2 != senha:
+                print("senha incorreta")
+                print("helloworld")
